e0e8a80db2d64966218cf78c6f56ece3ef40da3d
gost <kishor81160@gmail.com>
2021-12-31 22:02:03 +0530

    Some Basic Event Has Been Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
 import { ServerModule } from './server/server.module';
 import { UicomponentRoutingModule, UIroutingcomponent } from './uicomponent/uicomponent-routing.module';
 import { UicomponentModule } from './uicomponent/uicomponent.module';
 import { UserModule } from './user/user.module';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +17,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     UserModule,
+    AppRoutingModule,
     UicomponentModule,
     ServerModule,
     UicomponentRoutingModule,
